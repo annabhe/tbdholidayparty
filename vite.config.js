c1385201aa7226c473262e956d5146922d13dcd8
anna <annabhe@umich.edu>
2025-12-11 17:52:42 -0500

    vite config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/tbdholidayparty/",
+  base: "/tbdholidayparty",
+  publicDir: 'public',
   plugins: [react()],
 })
