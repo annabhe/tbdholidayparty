292b158a5d796f32795ed316698ea3e22bcbccc5
anna <annabhe@umich.edu>
2025-12-12 00:20:47 -0500

    Carousel autoscrolling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,10 @@
 import { Card, Text } from '@mantine/core';
 export default function Footer() {
 return (
-<Card mt="xl" p="md" radius="lg">
-<Text size="sm">Thanks for joining us for the ride! Can't wait to see you at the next one! 🚴‍♀️</Text>
-</Card>
+    <Card mt="xl" p="md" radius="lg">
+        <Text size="sm" style={{marginLeft: "15px", marginRight: "15px", marginBottom: "24px"}}>
+            Thanks for joining us for the ride! Can't wait to see you at the next one! 🚴‍♀️
+        </Text>
+    </Card>
 );
 }
